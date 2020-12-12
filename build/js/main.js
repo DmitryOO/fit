2381ff02cfdf0cb2933b14a26c5c8716d515db1b
DmitryOO <dimkut@mail.ru>
2020-12-13 01:48:56 +0400

    тренажерный зал

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,15 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
-
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+(function () {
+  var lastParagraph = document.querySelector('.gym__description p:last-child');
+  if (lastParagraph) {
+    var removeText = function () {
+      if (window.innerWidth < 1200) {
+        lastParagraph.style.display = 'none';
+      } else {
+        lastParagraph.style.display = 'inline';
+      }
+    };
+    removeText();
+    window.addEventListener('resize', removeText);
   }
-});
+}());
